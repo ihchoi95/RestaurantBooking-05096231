11b489db350b15f943ee55cb0e5170f1958f23d7
ihwa.choi <ihwa.choi@samsung.com>
2024-05-20 15:33:50 +0900

    [feature] update step9

--- RestaurantBooking-Test/test.cpp
+++ RestaurantBooking-Test/test.cpp
@@ -115,7 +115,15 @@ TEST_F(BookingItem, 이메일이_없는_경우에는_이메일_미발송) {
 }
 
 TEST_F(BookingItem, 이메일이_있는_경우에는_이메일_발송) {
+	// arrange 
+	Customer customerWithMail{ "Fake name", "010-1234-5678", "test@test.com"};
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, customerWithMail };
 
+	// act
+	bookingScheduler.addSchedule(schedule);
+
+	// assert
+	EXPECT_EQ(1, testableMailSender.getCountSendMailMethodIsCalled());
 }
 
 TEST_F(BookingItem, 현재날짜가_일요일인_경우_예약불가_예외처리) {
